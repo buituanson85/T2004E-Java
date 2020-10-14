39501223cf7c805294e516e220e7278dfca05572
buituanson85 <65030430+buituanson85@users.noreply.github.com>
2020-10-15 01:35:04 +0700

    class One

--- src/classOne.java
+++ src/classOne.java
@@ -2,5 +2,18 @@ public class classOne {
     public static void main(String[]args) {
         System.out.println("Name: Codelearn");
         System.out.println("Date of birth: 2019");
+        System.out.println("Hello, World!");
+        System.out.println("Name: Codelearn\nDate of birth: 2019\nHello, World!");
+        System.out.println("125 + 206 = " + (125 + 206));
+        System.out.println("2468 + 1234 = " + (2468 + 1234));
+        System.out.println("2468 - 1234 = " + (2468 - 1234));
+        System.out.println("2468 * 1234 = " + (2468 * 1234));
+        System.out.println("2468 / 1234 = " + (2468 / 1234));
+        int a = 6;
+        int b = 2;
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        System.out.println("a / b = " + (a / b));
     }
 }
