72972df189c53d4a622b397157ba51daf9f18ab5
buituanson85 <65030430+buituanson85@users.noreply.github.com>
2020-10-17 18:17:42 +0700

    Lession One

--- src/LessionOne/LessionOne.java
+++ src/LessionOne/LessionOne.java
@@ -7,7 +7,17 @@ public class LessionOne {
         Scanner sc = new Scanner(System.in);
         String name = sc.next();
         System.out.println("Tên tôi là: " + name);
-        System.out.printf("5/2 = %d %n", 5/2);
+        System.out.printf("5/2 = %f %n", 5/2f);
         System.out.println("5/2 = "+ 5/2);
+
+        int x;
+        x = 10;
+        int empNumber;
+        float salary;
+        double shareBalance;
+        char gender = 'M';
+        boolean ownVehicle = false;
+        empNumber = 101;
+        salary = 6789.50f;
     }
 }
