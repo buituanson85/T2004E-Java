72496dc0440be2bac649ff880e53f46861985a9e
buituanson85 <65030430+buituanson85@users.noreply.github.com>
2020-10-17 18:10:54 +0700

    Lession One

---
+++ src/LessionOne/LessionOne.java
@@ -0,0 +1,13 @@
+package LessionOne;
+
+import java.util.Scanner;
+
+public class LessionOne {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println("Tên tôi là: " + name);
+        System.out.printf("5/2 = %d %n", 5/2);
+        System.out.println("5/2 = "+ 5/2);
+    }
+}
